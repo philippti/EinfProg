d136d4c9987bcc421338d7e5edd84f9648198d6e
philippti <philippt@dhbw-loerrach.de>
2020-10-26 17:29:58 +0100

    first program

---
+++ about_me.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+date = datetime.now()
+
+
+jahr = input("Wann bist du geboren? ")
+wohnort = input("Wo wohnst du? ")
+farbe = input("Was ist deine Lieblingsfarbe? ")
+essen = input("Was ist dein Lieblingsessen? ")
+getraenk = input("Was ist dein Lieblingsgetraenk? ")
+
+
+
+print("† Du bist: " , int(date.year) - int(jahr))
+print("Du wohnst in: " , wohnort)
+
+print("       #")
+print("         #")
+print("        #")
+print("       #")
+print("       _")
+print("     _|=|__________")
+print("    /              \\")
+print("   /                \\")
+print("  /__________________\\")
+print("   ||  || /--\ ||  ||")
+print("   ||[]|| | .| ||[]||")
+print(" ()||__||_|__|_||__||()")
+print("^^^^^^^^^^====^^^^^^^^^^^")
+print()
+
+
+print("Deine Lieblingsfarbe ist: " , farbe)
+
+print()
+
+print("Am liebsten speist du: " , essen)
+
+print("                   _")
+print("                  / )")
+print("            |||| / /")
+print("            ||||/ /")
+print("            \\__(_/")
+print("            ||//")
+print("            ||/")
+print("            ||")
+print("           (||")
+print("            """)
+
+print("Am liebsten schlürfst du: " , getraenk)
+
+print('''   
+  .
+ . .
+  ...
+\~~~~~/
+ \   /
+  \ /
+   V
+   |
+   |
+  ---''')
+
+print("_" *30)
