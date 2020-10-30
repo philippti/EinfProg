91a14e702c0ccba618d1a85d45980f157e3d032c
philippti <philippt@dhbw-loerrach.de>
2020-10-30 15:20:11 +0100

    finished

--- fetchWeather/fetch_weather.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-import json
-from pprint import pprint
-
-url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/16646491'
-
-weather = get(url).json()['items']
-pprint(weather)
